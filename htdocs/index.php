2d729b1e053bddfd5dd2154cdbd807602f087050
fagianijunior <fagianijunior@hotmail.com>
2012-11-24 19:23:32 -0200

    Update htdocs/index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,10 +1,14 @@
 <html>
-   <head><title>Educa</title></head>
+   <head><title>{titulo}</title></head>
 <body>
-   <?php
-   $v1="test do git hub";
-   $v2=5+4;
-      echo $v1, $v2;
-   ?>
+   <section>
+		<article>
+			<header>
+				<h1>{artigo_titulo}</h1>
+			</header>
+			<div>{artigo_conteudo}</div>
+		</article>
+	</section>
+	<footer>{rodape}</footer>
 </body>
 </html>
