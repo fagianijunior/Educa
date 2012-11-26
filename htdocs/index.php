fa82afc4d9aae0d1b698df3d2c294e0a9ec498b9
Carlos Fagiani Junior <fagianijunior@hotmail.com>
2012-11-26 10:02:24 -0300

    login

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,35 +1,38 @@
 <html>
-   <head><title>{titulo}</title></head>
+<head>
+<title>{titulo}</title>
+</head>
 <body>
-   <?php
-      function getTemplate( $template, $pasta = "templates/" ) {
-        $arqTemp = $folder.$template; // criando var com caminho do arquivo
-	if ( is_file( $arqTemp ) ) { // verificando se o arq existe
-		return file_get_contents( $arqTemp ); // retornando conteúdo do arquivo
-	} else
-		return FALSE;
-        }
-
-function parseTemplate( $template, $array ) {
-	foreach ($array as $a => $b) { // recebemos um array com as tags
-		$template = str_replace( '{'.$a.'}', $b, $template );
+	<?php
+	function getTemplate( $template, $pasta = "templates/" ) {
+		$arqTemp = $folder.$template; // criando var com caminho do arquivo
+		if ( is_file( $arqTemp ) ) { // verificando se o arq existe
+			return file_get_contents( $arqTemp ); // retornando conteúdo do arquivo
+		} else
+			return FALSE;
 	}
-	return $template; // retorno o html com conteúdo final
-}
 
-$arrTags = array(
-	'titulo'=>'Dicas de PHP',
-	'artigo_titulo'=>'Templates em PHP - Dicas de PHP',
-	'artigo_conteudo'=>'Demonstração de como criar um sistema simples de templates com PHP',
-	'rodape'=>'© Dicas de PHP - Todos direitos reservados 2012 '
-);
+	function parseTemplate( $template, $array ) {
+		foreach ($array as $a => $b) { // recebemos um array com as tags
+			$template = str_replace( '{'.$a.'}', $b, $template );
+		}
+		return $template; // retorno o html com conteúdo final
+	}
 
-$template = getTemplate( 'templates/dicas.html' );
+	$arrTags = array(
+			'titulo'=>'Dicas de PHP',
+			'artigo_titulo'=>'Templates em PHP - Dicas de PHP',
+			'artigo_conteudo'=>'Demonstração de como criar um sistema simples de templates com PHP',
+			'rodape'=>'© Dicas de PHP - Todos direitos reservados 2012 '
+	);
 
-$templateFinal = parseTemplate( $template, $arrTags );
+	$template = getTemplate( 'templates/dicas.html' );
 
-echo $templateFinal;
-   <section>
+	$templateFinal = parseTemplate( $template, $arrTags );
+
+	echo $templateFinal;
+	?>
+	<section>
 		<article>
 			<header>
 				<h1>{artigo_titulo}</h1>
@@ -38,6 +41,5 @@ echo $templateFinal;
 		</article>
 	</section>
 	<footer>{rodape}</footer>
-	?>
 </body>
 </html>
